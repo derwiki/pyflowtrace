004f8a7753d4de5ae8b7b194b4bb01834ce474f4
Adam Derewecki <derwiki@yelp.com>
2010-04-29 11:15:48 -0700

    removed params from return lines

--- pyflowtrace.py
+++ pyflowtrace.py
@@ -13,52 +13,61 @@ for
 """
 
 import inspect
+import custom
 
 logfile = open("trace.flw", "w+")
 GREEN, BROWN, BLUE, MAGENTA, CYAN, WHITE = range(32, 38)
 
 def colorize(s, color=CYAN):
-    return "".join((chr(0033), '[1;%sm' % color, s, chr(0033), '[m'))
+	return "".join((chr(0033), '[1;%sm' % color, s, chr(0033), '[m'))
 
 def tracer(frame, event, arg):
-    if event in ('c_call', 'c_return', 'c_exception'):
-        return tracer
+	if event in ('c_call', 'c_return', 'c_exception'):
+		return tracer
 
-    excluded_functions = ("<module>", '__getattr__', '__init__', '<genexpr>', 'tofnamemoduleline', 'showstack', 'is_id_field', 'is_ids_field', 'convert_kv_pair', 'convert')
-    if frame.f_code.co_name in excluded_functions:
-        return tracer
+	excluded_functions = ("<module>", '__getattr__', '__init__', '<genexpr>', 'tofnamemoduleline', 'showstack', 'is_id_field', 'is_ids_field', 'convert_kv_pair', 'convert')
+	if frame.f_code.co_name in excluded_functions:
+		return tracer
 
-    # event is call or return (for setprofile)
-    line_nr = frame.f_code.co_firstlineno
-    fn_name = frame.f_code.co_name
-    filename = frame.f_code.co_filename
+	# event is call or return (for setprofile)
+	line_nr = frame.f_code.co_firstlineno
+	fn_name = frame.f_code.co_name
+	filename = frame.f_code.co_filename
 
-    # ignore builtin and library calls
-    if filename.startswith('/usr') or filename.startswith('/var/lib'):
-        return tracer
+	# ignore builtin and library calls
+	if filename.startswith('/usr') or filename.startswith('/var/lib'):
+		return tracer
 
-    # set nuisance files here, will be skipped
-    excluded_files = ()
+	# set nuisance files here, will be skipped
+	if hasattr(custom, "excluded_files"):
+		excluded_files = custom.excluded_files
+	else:
+	excluded_files = ()
 
-    for file in excluded_files:
-        if filename.endswith(file):
-            return tracer
+	for file in excluded_files:
+		if filename.endswith(file):
+			return tracer
 
-    stack_size = len(inspect.stack())
+	stack_size = len(inspect.stack())
 
-    # formatargvalues chokes on things not having self.data sometimes
-    try:
-        args = inspect.formatargvalues(*inspect.getargvalues(frame))
-    except Exception, e:
-        print e
-        return tracer
+	# formatargvalues chokes on things not having self.data sometimes
+	try:
+		args = inspect.formatargvalues(*inspect.getargvalues(frame))
+	except Exception, e:
+		print e
+		return tracer
 
-    FUNCTION_COLS = 120
-    PARAM_COLS = 160
+	FUNCTION_COLS = 120
+	PARAM_COLS = 260
 
-    function_section = " ".join(("|" * stack_size, event, fn_name, '[%s +%s]' % (colorize(filename), line_nr))).ljust(FUNCTION_COLS, '.')
-    buffer = "%s %s\n" % (function_section, args[:PARAM_COLS])
-    logfile.write(buffer)
-    logfile.flush()
+	function_section = " ".join(("|" * stack_size, event, fn_name, '[%s +%s]' % (colorize(filename), line_nr))).ljust(FUNCTION_COLS, '.')
 
-    return tracer
+	if event == "call":
+		param_section = args[:PARAM_COLS]
+	else:
+		param_section = ""
+	buffer = "%s %s\n" % (function_section, param_section)
+	logfile.write(buffer)
+	logfile.flush()
+
+	return tracer
