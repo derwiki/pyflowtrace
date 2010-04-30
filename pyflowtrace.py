ea4337be22b75326175d0bf7fc060de027930bcb
Adam Derewecki <derwiki@yelp.com>
2010-04-29 18:39:59 -0700

    some improvements

--- pyflowtrace.py
+++ pyflowtrace.py
@@ -21,6 +21,11 @@ GREEN, BROWN, BLUE, MAGENTA, CYAN, WHITE = range(32, 38)
 def colorize(s, color=CYAN):
 	return "".join((chr(0033), '[1;%sm' % color, s, chr(0033), '[m'))
 
+if hasattr(custom, 'cursor_variables'):
+	cursor_variables = custom.cursor_variables
+else:
+	cursor_variables = ('cursor', 'batch_cursor', 'rocursor')
+
 def tracer(frame, event, arg):
 	if event in ('c_call', 'c_return', 'c_exception'):
 		return tracer
@@ -50,9 +55,17 @@ def tracer(frame, event, arg):
 
 	stack_size = len(inspect.stack())
 
+
 	# formatargvalues chokes on things not having self.data sometimes
 	try:
-		args = inspect.formatargvalues(*inspect.getargvalues(frame))
+		raw_args = inspect.getargvalues(frame)
+		post_args = []
+		#TODO abstract out arg processing rules
+		for arg in raw_args:
+			if type(arg) == type({}):
+				arg = dict([(item, value) if item not in cursor_variables else (item, (value.connection.name, value)) for item, value in arg.items()])
+			post_args.append(arg)
+		args = inspect.formatargvalues(*post_args)
 	except Exception, e:
 		print e
 		return tracer
@@ -63,9 +76,10 @@ def tracer(frame, event, arg):
 	function_section = " ".join(("|" * stack_size, event, fn_name, '[%s +%s]' % (colorize(filename), line_nr))).ljust(FUNCTION_COLS, '.')
 
 	if event == "call":
-		param_section = args[:PARAM_COLS]
-	else:
-		param_section = ""
+		param_section = "params: %s" % args
+		#param_section = args[:PARAM_COLS]
+	elif event == "return":
+		param_section = "return: %s" % arg
 	buffer = "%s %s\n" % (function_section, param_section)
 	logfile.write(buffer)
 	logfile.flush()
