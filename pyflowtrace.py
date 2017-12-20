e227a33eaf3744da905702da2f07da0d87a99c12
Adam Derewecki <aderewecki@lyft.com>
2017-12-20 13:56:26 -0800

    brush your shoulder off

--- pyflowtrace.py
+++ pyflowtrace.py
@@ -6,14 +6,20 @@ sys.setprofile(pyflowtrace.tracer)
 Put that code where you want to begin your trace
 I recommend after imports are done to avoid clutter.
 
-'excluded_files' exists for you to filter out modules
+`excluded_files` exists for you to filter out modules
 that don't provide any useful information. Same goes
-for 
-
+for `excluded_functions`.
 """
 
 import inspect
-import custom
+
+has_custom = False
+try:
+    import custom
+    has_custom = True
+except ImportError:
+    pass
+
 
 logfile = open("trace.flw", "w+")
 GREEN, BROWN, BLUE, MAGENTA, CYAN, WHITE = range(32, 38)
@@ -24,7 +30,7 @@ def colorize(s, color=CYAN):
     # return "".join((chr(0033), '[1;%sm' % color, s, chr(0033), '[m'))
 
 
-if hasattr(custom, 'cursor_variables'):
+if has_custom and hasattr(custom, 'cursor_variables'):
     cursor_variables = custom.cursor_variables
 else:
     cursor_variables = ('cursor', 'batch_cursor', 'rocursor')
@@ -34,26 +40,38 @@ def tracer(frame, event, arg):
     if event in ('c_call', 'c_return', 'c_exception'):
         return tracer
 
-    excluded_functions = ("<module>", '__getattr__', '__init__', '<genexpr>', 'tofnamemoduleline',
-                          'showstack', 'is_id_field', 'is_ids_field', 'convert_kv_pair', 'convert')
-    if frame.f_code.co_name in excluded_functions:
-        return tracer
-
     # event is call or return (for setprofile)
     line_nr = frame.f_code.co_firstlineno
     fn_name = frame.f_code.co_name
     filename = frame.f_code.co_filename
 
+    excluded_functions = (
+      '<genexpr>',
+      '<module>',
+      '__getattr__',
+      '__init__',
+      '_remove',
+      'convert',
+      'convert_kv_pair',
+      'is_id_field',
+      'is_ids_field',
+      'showstack',
+      'tofnamemoduleline',
+    )
+
+    if frame.f_code.co_name in excluded_functions:
+        return tracer
+
     # ignore builtin and library calls
     if filename.startswith('/usr') or filename.startswith('/var/lib'):
         return tracer
 
-    if hasattr(custom, "file_prefix_ignore"):
+    if has_custom and hasattr(custom, 'file_prefix_ignore'):
         if filename.startswith(custom.file_prefix_ignore):
             filename = filename[len(custom.file_prefix_ignore):]
 
     # set nuisance files here, will be skipped
-    if hasattr(custom, "excluded_files"):
+    if has_custom and hasattr(custom, 'excluded_files'):
         excluded_files = custom.excluded_files
     else:
         excluded_files = ()
@@ -62,7 +80,10 @@ def tracer(frame, event, arg):
         if filename.endswith(file):
             return tracer
 
-    stack_size = len(inspect.stack())
+    if hasattr(inspect, 'stack'):
+        stack_size = len(inspect.stack())
+    else:
+        raise Exception("Add {} to `excluded_functions`".format(fn_name))
 
     # formatargvalues chokes on things not having self.data sometimes
     try:
@@ -70,7 +91,7 @@ def tracer(frame, event, arg):
         post_args = []
         # TODO abstract out arg processing rules
         for arg in raw_args:
-            if type(arg) == type({}):
+            if isinstance(arg, dict):
                 arg = dict([(item, value) if item not in cursor_variables else (
                     item, (value.connection.name, value)) for item, value in arg.items()])
             post_args.append(arg)
@@ -80,7 +101,6 @@ def tracer(frame, event, arg):
         return tracer
 
     FUNCTION_COLS = 90
-    PARAM_COLS = 260
 
     stack_ruler = "".join((str(i) for i in xrange(0, 9))) * 4
     function_section = " ".join((stack_ruler[:stack_size], event, fn_name, '[%s +%s]' % (
@@ -88,9 +108,9 @@ def tracer(frame, event, arg):
 
     if event == "call":
         param_section = "params: %s" % args
-        #param_section = args[:PARAM_COLS]
     elif event == "return":
         param_section = "return: %s" % arg
+
     buffer = "%s %s\n" % (function_section, param_section)
     logfile.write(buffer)
     logfile.flush()
