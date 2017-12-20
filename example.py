917f0560b841752f1079b044782f50420e2a1944
Adam Derewecki <aderewecki@lyft.com>
2017-12-20 13:47:27 -0800

    autopep8 example.py

--- example.py
+++ example.py
@@ -1,13 +1,17 @@
 import pyflowtrace
 import sys
 
+
 def fn_a(): fn_b(1, 2, "hello")
+
 
 def fn_b(a, b, c): fn_c(a, b, c, d=None, e=None)
 
+
 def fn_c(a, b, c, d=None, e=None, f=None):
-	pass
+    pass
+
 
 if __name__ == "__main__":
-	sys.setprofile(pyflowtrace.tracer)
-	fn_a()
+    sys.setprofile(pyflowtrace.tracer)
+    fn_a()
